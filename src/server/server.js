b0c175de2107a2085ff29af9f250cc8313262603
Vipul55 <Vipul.khanna@fiftyfivetech.io>
2023-04-19 17:08:27 +0530

    19/04/2023

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import config from '../../config/cofig.js'
-import routes from '../core/routes/index.js'
+import  bodyParser from 'body-parser'
+import database from '../core/database/database.js'
+// import authorized from '../core/routes/index.js'
+import unauthorized from '../core/routes/unauthorized/index.js';
 const app=express();
-routes(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+unauthorized(app);
 app.listen(config.PORT,()=>{console.log(`listening on port ${config.PORT}`)});
