2762f0a78d60f57896c20425caf7d6367a6ca17f
Sameer55tech <sameer.srivastava@fiftyfivetech.io>
2023-05-02 11:44:38 +0530

    add .env

--- config/config.js
+++ config/config.js
@@ -1,12 +1,15 @@
-const SECRET = "55ChitChat";
-const PORT = 8484;
-const SALT = 10;
-const DATABASE_NAME = "ChitChat";
-const DATA_USER_NAME = "postgres";
-const DATABASE_PASSWORD = "16122000";
-const DATABASE = "postgres";
-const DATABASE_HOST = "localhost";
-const JWT_EXPIRY = 86400;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const SECRET = process.env.SECRET;
+const PORT = process.env.PORT;
+const SALT = process.env.SALT;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATA_USER_NAME = process.env.DATA_USER_NAME;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DATABASE = process.env.DATABASE;
+const DATABASE_HOST = process.env.DATABASE_HOST;
+const JWT_EXPIRY = process.env.JWT_EXPIRY;
 
 export default {
   SECRET,
