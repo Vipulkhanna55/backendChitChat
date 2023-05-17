d23878edd1fb2c3974d564386e81a75a5512b067
Sameer55tech <sameer.srivastava@fiftyfivetech.io>
2023-05-17 16:57:28 +0530

    update token.js in testHelper

--- src/core/testHelper/token.js
+++ src/core/testHelper/token.js
@@ -1,7 +1,18 @@
 import server from "../../server/index.js";
 import request from "supertest";
 
+const createUser = async () => {
+  const response = await request(server).post("/signup").send({
+    firstName: "Sameer",
+    lastName: "Srivastava",
+    gender: "Male",
+    email: "sam895@gmail.com",
+    password: "Sam123@@@",
+  });
+}
+
 export default beforeAll = async () => {
+  await createUser();
   var authToken;
   const response = await request(server).post("/login").send({
     email: "sam895@gmail.com",
